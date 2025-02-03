a0ed351c24a1c82a7405459c13620c0a9abae973
Isaac Loh <89275242+isaaccloh@users.noreply.github.com>
2025-02-03 13:45:44 -0500

    Update Feb_3.R

--- Feb_3.R
+++ Feb_3.R
@@ -1,7 +1,17 @@
+# February 3
 rm(list = ls())
 
 install.packages('foreign')
 
 library(foreign)
+library(dplyr)
 
-mydata = read.dta("https://www.princeton.edu/~otorres/Panel101.dta")
+# Creating data from online dataset 
+df = read.dta("https://www.princeton.edu/~otorres/Panel101.dta")
+
+View(df)
+
+# DiD estimation of treatment effect
+df = df %>% mutate(time = ifelse(year >= 1994, 1, 0)) %>%
+  mutate(treated = ifelse(country == 'E' | country == 'F' | country == 'G', 1, 0)) %>%
+  mutate(did = time * treated)
